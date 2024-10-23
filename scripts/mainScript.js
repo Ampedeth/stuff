f2a89197946953289500328bd866acc2a17f1fd9
Anton <gav.fadt@gmail.com>
2024-10-23 14:59:27 +0200

    main page

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -1,3 +1,26 @@
+// Shop script part
+
+// Alerts
+
+  // Модальне вікно About Us
+  document.getElementById("about-us-btn").addEventListener("click", function () {
+    document.getElementById("about-us-modal").style.display = "flex";
+  });
+
+  // Закриття модальних вікон
+  document.getElementById("close-about").addEventListener("click", function () {
+      document.getElementById("about-us-modal").style.display = "none";
+  });
+
+  // Закриття модальних вікон при кліку на затемнення
+  window.addEventListener("click", function (event) {
+      if (event.target.classList.contains("modal")) {
+          event.target.style.display = "none";
+      }
+  });
+
+
+
 // Login script part
 
 let loginForm = document.querySelector(".my-form");
@@ -13,4 +36,3 @@ loginForm.addEventListener("submit", (e) => {
 
 
 
-// Sign up script part
